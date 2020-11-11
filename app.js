58ff6f068e65ff614638686d79328ffef553f42c
jenzhng <zhongjenn@gmail.com>
2020-11-11 03:32:48 -0500

    update app.js
    added dark sky icons

--- app.js
+++ app.js
@@ -1,49 +1,110 @@
-function startSkycons () {
-	
-	var icons = new Skycons ({"color": "orange"});
-	
-	icons.set("clear-day", Skycons.CLEAR_DAY);
-	icons.set("clear-night", Skycons.CLEAR_NIGHT);
-	icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
-	icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
-	icons.set("cloudy", Skycons.CLOUDY);
-	icons.set("rain", Skycons.RAIN);
-	icons.set("sleet", Skycons.SLEET);
-	icons.set("snow", Skycons.SNOW);
-	icons.set("wind", Skycons.WIND);
-	icons.set("fog", Skycons.FOG);
-
-	 switch (weatherIcon) {
-        case "01d":
-          skycons.add(document.getElementById("weatherIcon"), Skycons.CLEAR_DAY);
-          break;
-        case "01n":
-          skycons.add(document.getElementById("weatherIcon"), Skycons.CLEAR_NIGHT);
-          break;
-        case "02d":
-//var fahrenheit;
 var fahrenheitArr = [];
-var $icons = $(".icon");
-var currentIcon;
-
-//var $day = $(".day");
 
 
-function startSkycons(currentIcon){
-	//"https://rawgithub.com/darkskyapp/skycons/master/skycons.js"
+function weather() {
+	var code = document.getElementById("apikey").value;
+	var zip = document.getElementById("zipcode").value;
+	var url = "https://api.darksky.net/forecast/";
+	var latitude;
+	var longitude;
+	var $day = $(".day");
+	var $icons = $(".icon");
+	var UNIX_timestamp;
+	var $temp = $(".temp");
+
+
+
+	$.getJSON("https://raw.githubusercontent.com/jkeefe/better-weather-alexa/master/data/zips.json", function(data) {
+		$.each(data, function(key, val) {
+			if (key == zip) {
+				latitude = val.lat;
+				longitude = val.lon;
+
+				console.log(latitude);
+				console.log(longitude);
+				$.getJSON(
+					url + code + "/" + latitude + "," + longitude + "?callback=?",
+					function(data) {
+						//$("#temperature").html(data.currently.temperature + "° F");
+						//$("#minutely").html(data.minutely.summary);
+						//$("#hourly").html(data.hourly.icon);
+						console.log($day.length);
+						console.log($icons.length);
+						//var time = new TimeConverter(data.currently.time);
+						//$day.first().html(time.day + " " + time.dayNum);
+						
+						
+
+						
+						for (i = 0; i < $day.length; i++) {
+						// date
+							console.log(data.daily.data[i].time);
+							var time = new TimeConverter(data.daily.data[i].time);
+							$($day[i]).html(time.dayShort + " " + time.dayNum);
+						//	var icon
+						// temp
+							//var iconpicture = data.daily.data[i].icon;
+							//console.log(iconpicture);
+							getIcon(data.daily.data[i].icon, $icons[i]);
+							fahrenheit = data.daily.data[i].temperatureMax;
+							storeTemp(fahrenheit);
+							$($temp[i]).html(fahrenheitArr[i] + " °F");						    
+							
 	
-	var icon = currentIcon;
-	var skycons = new Skycons({"color": "black"});
-		/*skycons.set("icon", Skycons.RAIN);
-		skycons.set("icon", Skycons.CLEAR_DAY);
-		skycons.set("icon", Skycons.CLEAR_NIGHT);
-		skycons.set("icon", Skycons.PARTLY_CLOUDY_DAY);
-		skycons.set("icon", Skycons.PARTLY_CLOUDY_NIGHT);
-		skycons.set("icon", Skycons.CLOUDY);
-		skycons.set("icon", Skycons.SLEET);
-		skycons.set("icon", Skycons.SNOW);
-		skycons.set("icon", Skycons.WIND);
-		skycons.set("icon", Skycons.FOG);
+						}
+							
+						
+						$icons.each(function () {
+							this.width = 128;
+							this.height = 128;
+						});
+												
+					}
+				);
+			}
+			
+		});
+	}
+	);
+
+	
+
+}
+
+function getIcon(icon, iconID) {
+  const skycons = new Skycons({color: "black"});
+  var currentIcon = icon.replace(/-/g, '_').toUpperCase();
+  
+  skycons.play();
+  
+  console.log(currentIcon);
+  return skycons.set(iconID, Skycons[currentIcon]);
+  
+}
+
+function TimeConverter(UNIX_timestamp) {
+  var date = new Date(UNIX_timestamp * 1000);
+  var week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var weekShort = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  this.day = week[date.getDay()];
+  this.dayNum = date.getDate();
+  this.dayShort = weekShort[date.getDay()];
+}
+
+
+
+function storeTemp(fahrenheit) {
+  var tempf = Math.round(fahrenheit);
+  fahrenheitArr.push(tempf);
+}
+
+startSkycons(currentIcon);
+TimeConverter(UNIX_timestamp);
+getIcon(icon, iconID);
+storeTemp(fahrenheit);
+weather();
+
+
 	*/
 	switch (icon) {
 		case "CLEAR_DAY":
